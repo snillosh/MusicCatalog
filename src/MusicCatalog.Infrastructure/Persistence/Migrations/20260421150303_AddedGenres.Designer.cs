6070a18dbc58e7d992ac89e3ac47700dc2a3768b
bevan <bevansalter@proton.me>
2026-04-21 16:05:14 +0100

    Added genre migrations

--- src/MusicCatalog.Infrastructure/Persistence/Migrations/20260421150303_AddedGenres.Designer.cs
+++ src/MusicCatalog.Infrastructure/Persistence/Migrations/20260421150303_AddedGenres.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MusicCatalog.Infrastructure.Persistence;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MusicCatalog.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(MusicCatalogDbContext))]
-    partial class MusicCatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260421150303_AddedGenres")]
+    partial class AddedGenres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/MusicCatalog.Infrastructure/Persistence/Migrations/20260421150303_AddedGenres.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicCatalog.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedGenres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "genre",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_genre_Title",
+                table: "genre",
+                column: "Title",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "genre");
+        }
+    }
+}
