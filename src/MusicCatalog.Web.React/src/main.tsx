91fa040daf9e0fef4a3b9ebf6f164ea4fb2bf75d
bevan <bevansalter@proton.me>
2026-06-03 07:22:52 +0100

    Initial react project commit

---
+++ src/MusicCatalog.Web.React/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import './index.css'
+import App from './App.tsx'
+import {MantineProvider} from "@mantine/core";
+import {Notifications} from "@mantine/notifications";
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <MantineProvider>
+          <Notifications />
+          <App />
+      </MantineProvider>
+  </StrictMode>,
+)
