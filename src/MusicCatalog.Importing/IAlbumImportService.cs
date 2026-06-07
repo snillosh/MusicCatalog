622b231fa9f7a28453aad689549606f2df0f47ca
bevan <bevansalter@proton.me>
2026-06-07 13:24:54 +0100

    Removing old blazor files and tests

--- src/MusicCatalog.Importing/IAlbumImportService.cs
+++
@@ -1,8 +0,0 @@
-using MusicCatalog.Contracts.Albums;
-
-namespace MusicCatalog.Importing;
-
-public interface IAlbumImportService
-{
-    Task<AlbumDto> ImportAlbumAsync(AlbumImportPreview selectedAlbum, CancellationToken cancellationToken = default);
-}
